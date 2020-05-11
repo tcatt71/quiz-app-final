05f3c471e5bf9a06f6721deaedecd5b4df801dc5
Troy Coffindaffer <topcatt71@msn.com>
2020-05-10 19:51:54 -0700

    add an array of quiz questions

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
 'use strict';
 
+const question1 = {
+    question: "Who is the Seahawks current head coach?",
+    optiionA: { answer: "Bill Belicheck", isCorrect: false },
+    optiionB: { answer: "Chuck Knox", isCorrect: false },
+    optiionC: { answer: "Pete Carroll", isCorrect: true },
+    optiionD: { answer: "Micheal Jordan", isCorrect: false }
+};
+
+const question2 = {
+    question: "How many Superbowls have the Seahawks won?",
+    optiionA: { answer: "3", isCorrect: false },
+    optiionB: { answer: "6", isCorrect: false },
+    optiionC: { answer: "1", isCorrect: true },
+    optiionD: { answer: "none", isCorrect: false }
+};
+
+const question3 = {
+    question: "What was the first year the Seahawks made the playoffs?",
+    optiionA: { answer: "2012", isCorrect: false },
+    optiionB: { answer: "1983", isCorrect: true },
+    optiionC: { answer: "1976", isCorrect: false },
+    optiionD: { answer: "The Seahawks have never made the playoffs", isCorrect: false }
+};
+
+const question4 = {
+    question: "Who was the first starting quarterback for your Seattle Seahawks?",
+    optiionA: { answer: "Jon Kitna", isCorrect: false },
+    optiionB: { answer: "Tom Brady", isCorrect: false },
+    optiionC: { answer: "Russell Wilson", isCorrect: false },
+    optiionD: { answer: "Jim Zorn", isCorrect: true }
+};
+
+const question5 = {
+    question: "What Seahawk has the nickname 'Beast Mode'?",
+    optiionA: { answer: "Marshawn Lynch", isCorrect: true },
+    optiionB: { answer: "Kam Chancellor", isCorrect: false },
+    optiionC: { answer: "Chris Carson", isCorrect: false },
+    optiionD: { answer: "Blitz", isCorrect: false },
+};
+
+const questions = [question1, question2, question3, question4, question5];
+
 function handleStartQuizClicked() {
     //Listen for when a user clicks the 'Start' button.
-    //Load 1st questions object into the questions.html file.
     //Update Window with questions.html file.
     console.log('start quiz');
 }
@@ -36,7 +77,7 @@ function handleExitClicked() {
 
 
 
-function handleQuizApp() {
+/* function handleQuizApp() {
     handleStartQuizClicked();
     handleSubmitAnswerSubmit();
     handleNextQuestionClicked();
@@ -44,4 +85,4 @@ function handleQuizApp() {
     handleExitClicked();
 }
 
-$(handleQuizApp);
+$(handleQuizApp); */
